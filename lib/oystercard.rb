4cc97008563b9b1f7e489104b0dad5697361c3fc
eagle310 <schand9639@gmail.com>
2019-10-07 14:57:19 +0100

    question 6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,21 @@
 class Oystercard
 
+  MAX_BALANCE = 90
   attr_reader :balance
+  attr_reader :max_balance
 
   def initialize
     @balance = 0
+    @max_balance = MAX_BALANCE
+  end
+
+  def top_up(deposit)
+    fail 'Max balance reached' if full?(deposit)
+    @balance += deposit
+  end
+
+  def full?(deposit)
+    (@balance + deposit) > max_balance
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,7 +4,17 @@ describe Oystercard do
 
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
+  end
 
+  it 'adds money to balance' do
+    deposit = rand(10)
+    subject.top_up(deposit)
+    expect(subject.balance).to eq(deposit)
+  end
+
+  it 'has a max balance' do
+    subject.top_up(80)
+    expect { subject.top_up(15) }.to raise_error 'Max balance reached'
   end
 
 end
