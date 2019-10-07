84d4ba465bacf2adccf01c96f097e36de84d6ec0
Student <Student@Admins-MacBook-Air.local>
2019-10-07 16:00:17 +0100

    completed 9

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,16 @@
 class Oystercard
 
   MAX_BALANCE = 90
+  MIN_BALANCE = 1
   attr_reader :balance
   attr_reader :max_balance
+  attr_reader :min_balance
 
   def initialize
     @balance = 0
     @max_balance = MAX_BALANCE
+    @min_balance = MIN_BALANCE
+    @in_or_out = false
   end
 
   def top_up(deposit)
@@ -14,8 +18,25 @@ class Oystercard
     @balance += deposit
   end
 
+  def deduct(fare)
+    @balance -= fare
+  end
+
   def full?(deposit)
     (@balance + deposit) > max_balance
+  end
+
+  def touch_in
+    fail 'Not enough funds' if (@balance < min_balance)
+    @in_or_out = true
+  end
+
+  def touch_out
+    @in_or_out = false
+  end
+
+  def in_journey?
+    @in_or_out
   end
 
 end
